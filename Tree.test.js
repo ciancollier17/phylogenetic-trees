6784d66b32ac7cede2101cae3e6a0448b28d807f
Cian Collier <ciancollier17@gmail.com>
2020-01-19 12:36:03 +0000

    Added tests for Tree mergeNodes method

--- Tree.test.js
+++ Tree.test.js
@@ -29,3 +29,22 @@ test("Can convert tree with depth of 3 to Newick format", () => {
   const tree = new Tree([['A', 1], [[['B', 3], [[['C', 5], ['D', 6]], 4]], 2]]);
   expect(tree.toNewick()).toEqual("(A:1,(B:3,(C:5,D:6):4):2);");
 });
+
+test("Can merge single pair of nodes in tree", () => {
+  const tree = new Tree([['A', 1], ['B', 1], ['C', 1]]);
+  tree.mergeNodes('A', 'B', 'D', 2);
+  expect(tree.root.children.length).toEqual(2);
+  expect(tree.root.children[1].name).toEqual('D');
+  expect(tree.root.children[1].branchLength).toEqual(2);
+  expect(tree.toNewick()).toEqual("(C:1,(A:1,B:1):2);");
+});
+
+test("Can merge tree twice to create a tree with depth of 3", () => {
+  const tree = new Tree([['A', 1], ['B', 1], ['C', 1], ['D', 1]]);
+  tree.mergeNodes('A', 'B', 'E', 2);
+  tree.mergeNodes('E', 'C', 'F', 3);
+  expect(tree.root.children.length).toEqual(2);
+  expect(tree.root.children[1].name).toEqual('F');
+  expect(tree.root.children[1].branchLength).toEqual(3);
+  expect(tree.toNewick()).toEqual("(D:1,((A:1,B:1):2,C:1):3);");
+});
